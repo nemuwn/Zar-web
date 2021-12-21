774e1231d49ea032cb93d81d57d90e764c65ba3a
Naba <lolnarka12@gmail.com>
2021-12-21 19:43:39 +0000

    feat: project created

---
+++ next.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const withPlugins = require('next-compose-plugins');
+const withOffline = require('next-offline');
+
+/** @type {import('next').NextConfig} */
+module.exports = {
+  swcMinify: true,
+};
+
+module.exports = withPlugins(
+  [
+    [
+      withOffline,
+      {
+        workboxOpts: {
+          runtimeCaching: [
+            {
+              urlPattern: /.(png|jpe?g)$/,
+              handler: 'CacheFirst',
+            },
+          ],
+        },
+      },
+    ],
+  ],
+  {
+    experimental: {
+      scrollRestoration: true,
+    },
+    webpack: (config) => {
+      config.resolve.alias['@'] = path.join(__dirname, 'src');
+      return config;
+    },
+    reactStrictMode: true,
+  },
+);
